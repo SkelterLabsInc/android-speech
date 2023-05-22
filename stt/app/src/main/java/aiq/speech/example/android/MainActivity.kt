2944cf433b2d706d405f56c80b26d54765cfca22
Jinhyeon Kim <jinhyeonkim@wisefour.com>
2023-05-22 17:46:17 +0900

    git subrepo clone git@github.com:SkelterLabsInc/android-speech.git poodle/samples/android/kotlin
    subrepo:
      subdir:   "poodle/samples/android/kotlin"
      merged:   "df8ba0e4803"
    upstream:
      origin:   "git@github.com:SkelterLabsInc/android-speech.git"
      branch:   "master"
      commit:   "df8ba0e4803"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "110b9eb"
    
    Change-Id: I5157517b3e8ac6754de299aa450779c403f32418

--- stt/app/src/main/java/aiq/speech/example/android/MainActivity.kt
+++ stt/app/src/main/java/aiq/speech/example/android/MainActivity.kt
@@ -36,210 +36,223 @@ import java.util.concurrent.Executor
 import javax.net.ssl.SSLContext
 
 class MainActivity : AppCompatActivity(), StreamObserver<StreamingRecognizeResponse> {
-    private var isRecording = false
-    private var channel: ManagedChannel? = null
-    private var recognizeStream: StreamObserver<StreamingRecognizeRequest>? = null
-    private var audioTrack: AudioTrack? = null
+  private var isRecording = false
+  private var channel: ManagedChannel? = null
+  private var recognizeStream: StreamObserver<StreamingRecognizeRequest>? = null
+  private var audioTrack: AudioTrack? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // Add a click handler to start button
-        findViewById<Button>(R.id.start_button)?.setOnClickListener {
-            tryStart()
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    // Add a click handler to start button
+    findViewById<Button>(R.id.start_button)?.setOnClickListener {
+      tryStart()
+    }
+
+    // Add a click handler to stop button
+    findViewById<Button>(R.id.stop_button)?.setOnClickListener {
+      stop()
+    }
+  }
+
+  private fun tryStart() {
+    if (checkSelfPermission(this, RECORD_AUDIO) != PERMISSION_GRANTED) {
+      requestPermissions(arrayOf(RECORD_AUDIO), REQUEST_RECORD_AUDIO)
+    } else {
+      start()
+    }
+  }
+
+  private fun start() {
+    if (isRecording) {
+      makeText(this, "Already running", Toast.LENGTH_LONG).show()
+      return
+    }
+    startGrpc()
+    startAudioIn()
+    startAudioOut()
+  }
+
+  private fun stop() {
+    stopGrpc()
+    stopAudioIn()
+    stopAudioOut()
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray,
+  ) {
+    Log.d(TAG, "Receive the permission: $requestCode")
+    when (requestCode) {
+      REQUEST_RECORD_AUDIO -> if (grantResults.any { it == PERMISSION_GRANTED }) {
+        start()
+      } else {
+        makeText(this, "MIC permission needed", Toast.LENGTH_LONG).show()
+      }
+    }
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+  }
+  private fun startAudioIn() {
+    isRecording = false
+    val recorder = Runnable {
+      val samplingRate = 16_000
+      val bufferSize =
+        AudioRecord.getMinBufferSize(samplingRate, CHANNEL_IN_MONO, ENCODING_PCM_16BIT)
+          .takeUnless { it == ERROR || it == ERROR_BAD_VALUE } ?: (2 * samplingRate)
+      val audioRecord =
+        AudioRecord(MIC, samplingRate, CHANNEL_IN_MONO, ENCODING_PCM_16BIT, bufferSize)
+      if (audioRecord.state == STATE_INITIALIZED) {
+        audioRecord.startRecording()
+
+        val shortArray = ShortArray(bufferSize / 2)
+        isRecording = true
+        while (isRecording) {
+          val nRead = audioRecord.read(shortArray, 0, shortArray.size)
+
+          val buffer = ByteBuffer.allocate(2 * nRead).also { it.order(LITTLE_ENDIAN) }
+          buffer.asShortBuffer().put(shortArray, 0, nRead)
+          val bytes = buffer.array()
+          val req = StreamingRecognizeRequest.newBuilder()
+            .apply { audioContent = ByteString.copyFrom(bytes) }
+            .build()
+          recognizeStream?.onNext(req)
         }
+        audioRecord.stop()
+        audioRecord.release()
+      }
+    }
+    Thread(recorder).start()
+  }
 
-        // Add a click handler to stop button
-        findViewById<Button>(R.id.stop_button)?.setOnClickListener {
-            stop()
+  private fun stopAudioIn() {
+    isRecording = false
+  }
+
+  private fun startGrpc() {
+    channel = OkHttpChannelBuilder.forAddress(HOSTNAME, PORT)
+      .maxInboundMessageSize(16 * 1024 * 1024)
+      .overrideAuthority(HOSTNAME)
+      .useTransportSecurity()
+      .sslSocketFactory(
+        SSLContext.getInstance("TLSv1.2").apply { init(null, null, null) }.socketFactory,
+      )
+      .build()
+    val stub = SpeechGrpc.newStub(channel).withCallCredentials(object : CallCredentials() {
+      override fun applyRequestMetadata(
+        requestInfo: RequestInfo?,
+        appExecutor: Executor?,
+        applier: MetadataApplier,
+      ) {
+        runCatching {
+          applier.apply(
+            Metadata().apply {
+              put(
+                Metadata.Key.of(
+                  "x-api-key",
+                  Metadata.ASCII_STRING_MARSHALLER,
+                ),
+                API_KEY,
+              )
+            },
+          )
         }
+          .onFailure { applier.fail(Status.UNAUTHENTICATED.withCause(it)) }
+          .getOrNull()
+      }
+
+      override fun thisUsesUnstableApi() {
+      }
+    })
+
+    val stream = stub.streamingRecognize(this)
+    val streamingRecognitionConfig = StreamingRecognitionConfig.newBuilder()
+      .apply {
+        config = RecognitionConfig.newBuilder()
+          .apply { encoding = RecognitionConfig.AudioEncoding.LINEAR16 }
+          .apply { sampleRateHertz = 16_000 }
+          .apply { languageCode = "ko-KR" }
+          .build()
+      }
+      .build()
+    stream.onNext(
+      StreamingRecognizeRequest.newBuilder()
+        .apply { streamingConfig = streamingRecognitionConfig }
+        .build(),
+    )
+    recognizeStream = stream
+  }
+
+  private fun stopGrpc() {
+    val backup = channel
+    channel = null
+    backup?.shutdown()
+  }
+
+  /**
+   * Start speaker for audio-out
+   */
+  private fun startAudioOut() {
+    val samplingRate = 16_000
+    val bufferSize =
+      AudioTrack.getMinBufferSize(samplingRate, CHANNEL_OUT_MONO, ENCODING_PCM_16BIT)
+        .takeUnless { it == ERROR || it == ERROR_BAD_VALUE }
+        ?: (samplingRate * 2)
+    audioTrack = AudioTrack.Builder()
+      .setAudioAttributes(
+        AudioAttributes.Builder()
+          .setUsage(USAGE_MEDIA)
+          .setContentType(CONTENT_TYPE_MUSIC)
+          .build(),
+      )
+      .setAudioFormat(
+        AudioFormat.Builder()
+          .setEncoding(ENCODING_PCM_16BIT)
+          .setSampleRate(samplingRate)
+          .setChannelMask(CHANNEL_OUT_MONO)
+          .build(),
+      )
+      .setBufferSizeInBytes(bufferSize)
+      .setTransferMode(MODE_STREAM)
+      .build()
+    audioTrack?.play()
+  }
+
+  /**
+   * Stop speaker for audio-out
+   */
+  private fun stopAudioOut() {
+    val backup = audioTrack
+    audioTrack = null
+    backup?.stop()
+    backup?.release()
+  }
+
+  override fun onNext(res: StreamingRecognizeResponse) {
+    val resultText = findViewById<TextView>(R.id.result_text)
+    res.resultsList.forEach { result -> resultText.append(result.getAlternatives(0).transcript + "\n") }
+  }
+
+  override fun onError(t: Throwable) {
+    Log.e(TAG, "Unexpected exception", t)
+    stopGrpc()
+  }
+
+  override fun onCompleted() {
+    Log.d(TAG, "$this completed")
+    stopGrpc()
+  }
+
+  companion object {
+    const val REQUEST_RECORD_AUDIO = 300
+    const val API_KEY = "<<Your API Key>>"
+    const val HOSTNAME = "aiq.skelterlabs.com"
+    const val PORT = 443
+    const val TAG = "speech-android"
+
+    init {
+      Security.insertProviderAt(Conscrypt.newProvider(), 1)
     }
-
-    private fun tryStart() {
-        if (checkSelfPermission(this, RECORD_AUDIO) != PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(RECORD_AUDIO), REQUEST_RECORD_AUDIO)
-        } else {
-            start()
-        }
-    }
-
-    private fun start() {
-        if (isRecording) {
-            makeText(this, "Already running", Toast.LENGTH_LONG).show()
-            return
-        }
-        startGrpc()
-        startAudioIn()
-        startAudioOut()
-    }
-
-    private fun stop() {
-        stopGrpc()
-        stopAudioIn()
-        stopAudioOut()
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) {
-        Log.d(TAG, "Receive the permission: $requestCode")
-        when (requestCode) {
-            REQUEST_RECORD_AUDIO -> if (grantResults.any { it == PERMISSION_GRANTED }) {
-                start()
-            } else {
-                makeText(this, "MIC permission needed", Toast.LENGTH_LONG).show()
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-    private fun startAudioIn() {
-        isRecording = false
-        val recorder = Runnable {
-            val samplingRate = 16_000
-            val bufferSize =
-                    AudioRecord.getMinBufferSize(samplingRate, CHANNEL_IN_MONO, ENCODING_PCM_16BIT)
-                            .takeUnless { it == ERROR || it == ERROR_BAD_VALUE } ?: (2 * samplingRate)
-            val audioRecord =
-                    AudioRecord(MIC, samplingRate, CHANNEL_IN_MONO, ENCODING_PCM_16BIT, bufferSize)
-            if (audioRecord.state == STATE_INITIALIZED) {
-                audioRecord.startRecording()
-
-                val shortArray = ShortArray(bufferSize / 2)
-                isRecording = true
-                while (isRecording) {
-                    val nRead = audioRecord.read(shortArray, 0, shortArray.size)
-
-                    val buffer = ByteBuffer.allocate(2 * nRead).also { it.order(LITTLE_ENDIAN) }
-                    buffer.asShortBuffer().put(shortArray, 0, nRead)
-                    val bytes = buffer.array()
-                    val req = StreamingRecognizeRequest.newBuilder()
-                            .apply { audioContent = ByteString.copyFrom(bytes) }
-                            .build()
-                    recognizeStream?.onNext(req)
-                }
-                audioRecord.stop()
-                audioRecord.release()
-            }
-        }
-        Thread(recorder).start()
-    }
-
-    private fun stopAudioIn() {
-        isRecording = false
-    }
-
-    private fun startGrpc() {
-        channel = OkHttpChannelBuilder.forAddress(HOSTNAME, PORT)
-                .maxInboundMessageSize(16 * 1024 * 1024)
-                .overrideAuthority(HOSTNAME)
-                .useTransportSecurity()
-                .sslSocketFactory(
-                        SSLContext.getInstance("TLSv1.2").apply { init(null, null, null) }.socketFactory
-                )
-                .build()
-        val stub = SpeechGrpc.newStub(channel).withCallCredentials(object : CallCredentials() {
-            override fun applyRequestMetadata(
-                    requestInfo: RequestInfo?,
-                    appExecutor: Executor?,
-                    applier: MetadataApplier
-            ) {
-                runCatching { applier.apply(Metadata().apply { put(
-                        Metadata.Key.of("x-api-key",
-                                Metadata.ASCII_STRING_MARSHALLER
-                        ), API_KEY) }) }
-                        .onFailure { applier.fail(Status.UNAUTHENTICATED.withCause(it)) }
-                        .getOrNull()
-            }
-
-            override fun thisUsesUnstableApi() {
-            }
-        })
-
-        val stream = stub.streamingRecognize(this)
-        val streamingRecognitionConfig = StreamingRecognitionConfig.newBuilder()
-                .apply { config = RecognitionConfig.newBuilder()
-                            .apply { encoding = RecognitionConfig.AudioEncoding.LINEAR16 }
-                            .apply { sampleRateHertz = 16_000 }
-                            .apply { languageCode = "ko-KR" }
-                            .build() }
-                .build()
-        stream.onNext(StreamingRecognizeRequest.newBuilder()
-                .apply { streamingConfig = streamingRecognitionConfig }
-                .build())
-        recognizeStream = stream
-    }
-
-    private fun stopGrpc() {
-        val backup = channel
-        channel = null
-        backup?.shutdown()
-    }
-
-    /**
-     * Start speaker for audio-out
-     */
-    private fun startAudioOut() {
-        val samplingRate = 16_000
-        val bufferSize =
-                AudioTrack.getMinBufferSize(samplingRate, CHANNEL_OUT_MONO, ENCODING_PCM_16BIT)
-                        .takeUnless { it == ERROR || it == ERROR_BAD_VALUE }
-                        ?: (samplingRate * 2)
-        audioTrack = AudioTrack.Builder()
-                .setAudioAttributes(
-                        AudioAttributes.Builder()
-                                .setUsage(USAGE_MEDIA)
-                                .setContentType(CONTENT_TYPE_MUSIC)
-                                .build()
-                )
-                .setAudioFormat(
-                        AudioFormat.Builder()
-                                .setEncoding(ENCODING_PCM_16BIT)
-                                .setSampleRate(samplingRate)
-                                .setChannelMask(CHANNEL_OUT_MONO)
-                                .build()
-                )
-                .setBufferSizeInBytes(bufferSize)
-                .setTransferMode(MODE_STREAM)
-                .build()
-        audioTrack?.play()
-    }
-
-    /**
-     * Stop speaker for audio-out
-     */
-    private fun stopAudioOut() {
-        val backup = audioTrack
-        audioTrack = null
-        backup?.stop()
-        backup?.release()
-    }
-
-    override fun onNext(res: StreamingRecognizeResponse) {
-        val resultText = findViewById<TextView>(R.id.result_text)
-        res.resultsList.forEach{ result -> resultText.append(result.getAlternatives(0).transcript + "\n") }
-    }
-
-    override fun onError(t: Throwable) {
-        Log.e(TAG, "Unexpected exception", t)
-        stopGrpc()
-    }
-
-    override fun onCompleted() {
-        Log.d(TAG, "$this completed")
-        stopGrpc()
-    }
-
-    companion object {
-        const val REQUEST_RECORD_AUDIO = 300
-        const val API_KEY = "<<Your API Key>>"
-        const val HOSTNAME = "aiq.skelterlabs.com"
-        const val PORT = 443
-        const val TAG = "speech-android"
-
-        init {
-            Security.insertProviderAt(Conscrypt.newProvider(), 1);
-        }
-    }
+  }
 }
